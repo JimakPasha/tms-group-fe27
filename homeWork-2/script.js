2182931fd96c617233bfbe8d753b1a48a5d40466
Stepan <stewie_the.rofl@mail.ru>
2022-04-26 18:44:00 +0300

    task2 done

--- homeWork-2/script.js
+++ homeWork-2/script.js
@@ -6,3 +6,12 @@ let z = 42
 console.log (x + y + z);
 
 //2
+const sec = 60,
+min = 60,
+hour = 12,
+day = 365;
+
+let MyAgeInSeconds = (26 * sec * min * hour * day);
+
+console.log (MyAgeInSeconds);
+
