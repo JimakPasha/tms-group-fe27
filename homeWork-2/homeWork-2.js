621a41b2bc150035d9e7fab7e9b1582a5d5beabf
Artur-Gazaran <arturgazaran94@gmail.com>
2022-04-27 18:46:06 +0300

    init(hw-2): start course Javascript Basic

---
+++ homeWork-2/homeWork-2.js
@@ -0,0 +1,88 @@
+
+// Task1
+let x = 20;
+let y = 58;
+let z = 42;
+let f = (x+y+z);
+console.log(f);
+
+//Task2
+
+let secondInMinute = 60;
+let minuteInHour = 60;
+let hourInDay = 24;
+let dayInYear = 365;
+
+let myAge = 28;
+let myAgeInSeconds = 883008000;
+console.log(myAge * dayInYear * hourInDay * minuteInHour * secondInMinute);
+
+//Task3
+let count = 42
+let userName = '42'
+
+console.log(String(count));
+console.log(count+'');
+
+console.log(+userName);
+console.log(Number(userName));
+
+
+
+//Task4
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+
+console.log( a +'' + b +'' + ' ' + 'белых медведей');
+
+//Task5
+
+let one = 'доступ';
+let two = 'морпех';
+let three = 'наледь';
+let four = 'попрек';
+let five = 'рубило';
+let lengthWords = (one + two + three + four + five);
+console.log(lengthWords.leng);
+
+//Task6  ?????
+const namber = 13;
+const firstName = 'Toni';
+const boolean = true;
+
+console.log();
+console.log();
+console.log();
+
+
+//Task7
+
+const name1 = prompt('Как тебя зовут?');
+console.log(`Привет, ${name1}!`);
+
+const age = prompt('Сколько тебе лет?');
+console.log(`Привет, ${age}!`);
+
+
+//Task8
+
+let a1 = 4;
+let b1 = 3;
+console.log(a1, b1);
+[a1, b1] = [b1, a1];
+console.log(a1, b1);
+
+//Task9
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+console.log(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
+
+
+let cipher ='беги!';
+
