07528d7a19abd5451173ac7253375b0c4b3b6f2f
PAVEL <pashykpavel@gmail.com>
2022-04-27 09:07:26 +0400

    fix: bugs

--- homeWork-2/script.js
+++ homeWork-2/script.js
@@ -51,14 +51,14 @@ let lengthWords = {
    y: "рубило"
 };
 
-console.log(lengthWords.p.length + lengthWords.o.length + lengthWords.i.length + lengthWords.u.length + lengthWords.y.length)
+console.log(lengthWords.p.length + lengthWords.o.length + lengthWords.i.length + lengthWords.u.length + lengthWords.y.length);
 
 //Task 6
 
 const number = 1;
 console.log("Variable:", number, "have type:", typeof number);
 
-const text = "one"
+const text = "one";
 console.log("Variable:", text, "have type:", typeof text);
 
 const isTrue = true;
@@ -81,8 +81,8 @@ g = g + h /*складываем числа =7*/
 h = g - h /*говорим h, что оно = 4*/
 g = g - h /*говорим g, что оно = 3*/
 
-console.log(g)
-console.log(h)
+console.log(g);
+console.log(h);
 
 //Task 1.2
 
@@ -93,4 +93,4 @@ let codeWord4 = "липкие";
 let codeWord5 = "?!";
 
 let cipher = (codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
-console.log(cipher)
+console.log(cipher);
