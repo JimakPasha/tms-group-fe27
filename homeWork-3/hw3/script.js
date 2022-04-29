cbf5cd7b089367df8842c54e77526a04518c70f7
Dasha3518 <fedorovadasha3518@gmaol.com>
2022-04-29 23:36:40 +0300

    hw-3

---
+++ homeWork-3/hw3/script.js
@@ -0,0 +1,151 @@
+//task 1
+{
+    let str = 'true';
+    let bool = false;
+    let num = 17;
+    let und = undefined;
+    let zero =null;
+
+    console.log(typeof str, typeof bool, typeof num, typeof und, typeof zero);
+}
+
+//task 2 
+let height = 15;
+let width = 20;
+
+if (height > width) {
+    console.log(height)}
+    else  {
+    console.log(width)}
+
+// task 3
+
+let yy = 1;
+
+for (; yy <= 20; yy++) {
+if (yy % 3 == 0){
+    console.log(yy);
+}
+}
+
+
+// task 4 
+
+let key = true;
+let documents = true;
+let pen = true;
+let apple = false;
+let orange = true;
+
+let shouldGoToWork;
+
+if (key && documents && pen &&(apple || orange)) {
+    shouldGoToWork = "Yes, we can";
+    console.log(shouldGoToWork);
+} else {
+    shouldGoToWork = "No , we can't";
+    console.log(shouldGoToWork);
+}
+
+
+// task 5
+{
+    let num = prompt ('введите число');
+
+    if( num % 3 ==0 && num % 5 == 0){
+        alert ('FizBuz')
+    } else if (num % 5 == 0) {
+        alert('Fiz')
+    } else if ( num % 3 == 0) {
+        alert ('Buz')
+    } ;
+}
+
+// task 6 
+
+let userAge = prompt ('Сколько тебе лет?');
+if (userAge >= 18) {
+    alert ('попей пивка')
+} else if (userAge < 16) {
+    alert ('пей колу')
+} else if (userAge >= 16 && userAge < 18){
+    alert ('Можешь выкурить сигаретку, только маме не говори')
+};
+
+// task 7 
+
+let passed = false;
+
+while (passed == false) {
+    let country = prompt ('В какую сторону света Вы хотели бы отправиться');
+switch (country.toLocaleLowerCase()) {
+    case 'юг':
+        alert ('на юг пойдешь счастье найдешь');
+        passed = true;
+        break;
+    case 'север':
+        alert ('на север пойдешь много денег найдешь');
+        passed = true;
+        break;
+    case 'запад':
+        alert ('на запад пойдешь верного друга найдешь');
+        passed = true;
+        break;
+    case 'восток':
+        alert ('на восток пойдешь разработчиком станешь'); 
+        passed = true;
+        break;   
+    default:
+        alert ('Пройдите опрос еще раз');
+        break;
+}    
+}
+
+
+
+// task 1* пОлИнА нАбЕрЕжНаЯ 
+
+{
+    let userName = prompt ('Как Вас зовут?');
+    let temp = userName.toLowerCase();
+    let newName = ' ';
+
+    temp = temp.split(' ');
+
+    let i = 0;
+    for (; i < temp.length; i++){
+        let firstLette = temp[i][0].toUpperCase();
+        newName += firstLette+ temp[i].slice(1)+" ";
+        }
+    alert (`Привет, ${newName.trim()}!!!!`)
+}
+
+// task 2*
+
+let num = +prompt ('Введите число?');
+let minus = +prompt ('Сколько Вы хотите отнять от Вашего числа?');
+let plus = +prompt ('Теперь, мы прибавим к нему чиcло?') ;
+let multiply = +prompt ('Может умножим на что-нибудь?');
+let divide = 0 ;
+while (divide == 0){
+    divide = +prompt('И последнее, это чиcло мы разделим на Ваше число?');
+    if (divide == 0){
+            alert ('на 0 делить нельзя, учи математику')
+    }
+}
+let answer = (((((num - minus) + plus) * multiply) / divide));
+alert (answer) ;
+
+
+//task 3* 
+
+let lesenka = '';
+for (let i = 0; i < 6; i++) {
+    lesenka += '#';
+    console.log(lesenka);
+    }
+
+
+
+
+
