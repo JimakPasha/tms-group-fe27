730b09435f88fda9df1045fcf03acf7b047fbb6e
TilegenB <tilegen.bakitov@outlook.com>
2022-04-30 20:55:08 +0500

    add-hw(hw-3): Our-Product section

--- homeWork-3/organick/dist/style.a6dae8f7.js
+++ homeWork-3/organick/dist/style.a6dae8f7.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/cart.svg":[["cart.f72fc7e4.svg","img/cart.svg"],"img/cart.svg"],"_css_loader":"../../../../../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/cart.svg":[["cart.f72fc7e4.svg","img/cart.svg"],"img/cart.svg"],"./../img/star.svg":[["star.647382b2.svg","img/star.svg"],"img/star.svg"],"_css_loader":"../../../../../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54425" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59268" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
