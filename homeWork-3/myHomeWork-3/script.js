0120fad06d3b4fab031f06de09deb8ce32c27ebd
Work <liza@macbook-pro-sergej.home>
2022-05-03 22:36:51 +0200

    22.38

--- homeWork-3/myHomeWork-3/script.js
+++ homeWork-3/myHomeWork-3/script.js
@@ -1,3 +1,5 @@
+//1
+{
 
 let a = 'true';
 let b = false;
@@ -5,25 +7,38 @@ let c = 17;
 let d = undefined;
 let e =null;
 console.log(typeof a,typeof b,typeof c,typeof d,typeof e);
-    
-    
-    
+
+}   
+
+
+
+//2  
+{
+
 let height = 15;
 let width = 20;
 if (height<width){
     console.log(width)
 };
-    
 
+}  
+
+
+
+//3
+{
 let three=1;
 for (; three <= 20; three++) {
     if (three % 3 == 0){
         console.log(three);
     }
 }
+}
 
 
 
+//4
+{
 
 let key = true
 let documents = true
@@ -33,7 +48,7 @@ let orange = true
 
 let shouldGoToWork;
 
-if(key && documents && pen (apple || orange)){
+if(key && documents && pen && (apple || orange)){
     shouldGoToWork="не забудь фрукты";
     console.log(shouldGoToWork)
 }else{
@@ -41,17 +56,40 @@ if(key && documents && pen (apple || orange)){
     console.log(shouldGoToWork)
     }
 
+}
 
 
-let num = prompt ('введите число');
 
-if ( num % 3 == 0 && num % 5 == 0){
-    alert('FizBuz')
-} else if (num % 5 == 0) {
-    alert('Fiz')
-} else if ( num % 3 == 0) {
-    alert('Buz')
-    } ;
+//5
+{
+
+//let num = prompt('введите число');
+//if ( num % 3 == 0 && num % 5 == 0){
+//    alert('FizBuz')
+//} else if (num % 5 == 0) {
+ //   alert('Fiz')
+//} else if ( num % 3 == 0) {
+//    alert('Buz')
+//    } ;
+
+}
+
+
+
+//6
+{
+
+let age= prompt ("Введите свой возраст")
+if( age > 18 ){
+    alert('Попей пивка')
+}else if (age < 16){
+    alert('Пей колу')
+}else if (age >= 16 || age <= 18 ){
+    alert('Можешь выкурить сигаретку, только маме не говори')
+}
+
+
+}
 
 
 
