b98daa79d16050735a842d9f9acbb28994d72e48
Work <liza@macbook-pro-sergej.home>
2022-05-03 21:45:09 +0200

    hw-3

---
+++ homeWork-3/myHomeWork-3/script.js
@@ -0,0 +1,49 @@
+
+let a = 'true';
+let b = false;
+let c = 17;
+let d = undefined;
+let e =null;
+console.log(typeof a,typeof b,typeof c,typeof d,typeof e);
+    
+    
+    
+let height = 15;
+let width = 20;
+if (height<width){
+    console.log(width)
+};
+    
+
+let three=1;
+for (; three <= 20; three++) {
+    if (three % 3 == 0){
+        console.log(three);
+    }
+}
+
+
+
+
+let key = true
+let documents = true
+let pen = true
+let apple = false
+let orange = true
+
+let shouldGoToWork;
+
+if(key && documents && pen (apple || orange)){
+    shouldGoToWork="не забудь фрукты";
+    console.log(shouldGoToWork)
+}else{
+    shouldGoToWork="теперь можно идти ";
+    console.log(shouldGoToWork)
+    }
+
+
+
+
+
+
+
