210429a7eb04f47bbba25f3789c60fddb6890863
Work <liza@macbook-pro-sergej.home>
2022-05-10 00:11:52 +0200

    added task 1

---
+++ homeWork-4/sergei-mishyn-hw-4/script.js
@@ -0,0 +1,17 @@
+'use strict'
+{
+// task 1
+
+function getSum(sum) {
+    for (let i = 1; i < 100; i++) {
+        sum = sum + i;
+    }
+    console.log(sum)
+}
+getSum(100)
+// let myFn = getSum(prompt('Введите число'));
+// alert(myFn);  // почему тут в консоле в браузере выбивает undefined ?
+
+}
+
+// 2
