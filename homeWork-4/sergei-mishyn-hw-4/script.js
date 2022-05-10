ce6cf7ea48b427262e989586b763bea3229a07a8
Work <liza@macbook-pro-sergej.home>
2022-05-11 01:32:55 +0200

    added 1-5 tasks

--- homeWork-4/sergei-mishyn-hw-4/script.js
+++ homeWork-4/sergei-mishyn-hw-4/script.js
@@ -14,4 +14,66 @@ getSum(100)
 
 }
 
-// 2
+// task 2
+{
+
+function credit(sumCredit) {
+    let a = (sumCredit*0.17)*5;
+    console.log(a)
+}
+credit(100);
+
+}
+
+// task 3
+{
+
+function trimString(string,from,to) {
+    let a = string.slice(from,to);
+    console.log(a)
+}
+trimString('Привет',1,4);
+
+}
+
+// task 4
+
+{
+
+function countDigits(num) {
+    for(i = 0; num >= 1; i++) {
+       num /= 10;
+    }
+    console.log(i)
+ }
+
+countDigits(5555)
+
+
+}
+// task 5
+{
+
+function getSum(a, b) {
+    let c;
+    if (a < b) {
+        c = a;
+        for (let i = a + 1; i <= b; i++) {
+            c += i;
+        }
+        return c;
+    } else if (a > b) {
+        c = b;
+        for (let i = b + 1; i <= a; i++) {
+            c += i;
+        } 
+        return c;
+    } else {
+        return c = a || b;
+    }
+}
+
+
+console.log(getSum(1, 1));
+
+}
