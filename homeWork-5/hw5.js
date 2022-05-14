dd8406afd52b7b91ca08a404e518aac32cb7bbc4
Siarhei-Douhi <serrik23@gmail.com>
2022-05-14 18:34:47 +0300

    init: start hw5

---
+++ homeWork-5/hw5.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Task 1
+let car = {
+    audi: 'A8',
+    mersedes: 'w140',
+};
+console.log(car); // проверка свойств объекта
+delete car.audi;
+delete car.mersedes;
+console.log(car); // проверка свойств объекта после удаления
+
+// Task 2
