905a0fe9dfa650d8440b205c682c0b1a1e52e9be
Artur-Gazaran <arturgazaran94@gmail.com>
2022-05-17 20:02:19 +0300

    init(hw-5):home-work-5

--- home-work-5/hw-5.js
+++ home-work-5/hw-5.js
@@ -62,14 +62,46 @@ console.log(sum);
 
 
 //Task-6
-?
+let login = prompt('Введите логин');
+let password = prompt('Введите пороль');
 
+let loginPassword = {
+	login,
+	password,
+};
+
+let confirmLogin = prompt('Подтвердите логин');
+let confirmPassword = prompt('Подтвердите пороль');
+
+if(
+	loginPassword.login === confirmLogin && 
+	loginPassword.password === confirmPassword
+) {
+	alert('Авторизация выполнена');
+} else {
+	alert('Неверные данные');
+}
 
 //Advanced
 
 //Task-1
-?
-
+function getSoccerScore2(goalTim1, goalTim2) {
+	let numberString = {
+		1: 'one', 
+		2: 'two',
+		3: 'three',
+		4: 'fhour', 
+		5: 'five', 
+		6: 'six',
+		7: 'seven', 
+		8: 'eight', 
+		9: 'nine',
+		0: 'zero',
+};
+	return console.log(`${numberString[goalTim1]} : ${numberString[goalTim2]}`);
+}
+getSoccerScore2(0, 0);
+getSoccerScore2(5, 1);
 
 
 //Task-2
@@ -88,8 +120,16 @@ console.log(student1 === student2);
 
 
 //Task-3
-?
+const animals = {
+	cat: {
+		name: 'Енчик',
+		age: 3,
+	},
+	dog: {
+		name: 'Орео',
+		age: 2,
+	},
+	};
 
-
-
+	console.log(animals?.user?.name);
 
