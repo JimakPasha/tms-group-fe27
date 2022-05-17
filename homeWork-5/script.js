f54c9cf8536a2b6bf69e01b81d20f4699af6aaa0
PAVEL <pashykpavel@gmail.com>
2022-05-17 15:09:10 +0400

    fix: bugs #3.1 and #3.3

--- homeWork-5/script.js
+++ homeWork-5/script.js
@@ -5,17 +5,17 @@
 а затем удалить ключи из обьекта.*/
 
 {
-const petCat = {
-   name: "Sally",
-   age: 1,
-}
+   const petCat = {
+      name: "Sally",
+      age: 1,
+   }
 
-console.log(petCat);
+   console.log(petCat);
 
-delete petCat.name;
-delete petCat.age;
+   delete petCat.name;
+   delete petCat.age;
 
-console.log(petCat);
+   console.log(petCat);
 }
 
 /*Task 2
@@ -23,19 +23,19 @@ console.log(petCat);
 а затем проверить есть ли в обьекте определенный ключ и если есть вывести в консоль **true***/
 
 {
-let pets = {
-   cat: {
-      name: "Sally",
-      age: 1,
-   },
-   dog: {
-      name: "Lucky",
-      age: 2,
-   },
-}
+   let pets = {
+      cat: {
+         name: "Sally",
+         age: 1,
+      },
+      dog: {
+         name: "Lucky",
+         age: 2,
+      },
+   }
 
-console.log("cat" in pets);// определить ключ в Object 
-console.log(pets.dog.age !== undefined);// определить ключ в Object в Object
+   console.log("cat" in pets);// определить ключ в Object 
+   console.log(pets.dog.age !== undefined);// определить ключ в Object в Object
 }
 
 /*Task 3
@@ -43,31 +43,31 @@ console.log(pets.dog.age !== undefined);// определить ключ в Obje
 потом значения ключей обьекта.*/
 
 {
-const student = {
-   name: 'John',
-   age: 19,
-   isHappy: true,
-}
+   const student = {
+      name: 'John',
+      age: 19,
+      isHappy: true,
+   }
 
-for (let key in student) {
-console.log(key);// выводит ключи
-console.log(student[key]);// выводит значения ключей
-}
+   for (let key in student) {
+      console.log(key);// выводит ключи
+      console.log(student[key]);// выводит значения ключей
+   }
 }
 
 /*Task 4
 Дан обьект. Вывести в консоль слово красный и синий*/
 
 {
-const colors = {
-   'ru pum pu ru rum': {
-      red: 'красный',
-      green: 'зеленый',
-      blue: 'синий',
-   },
-};
+   const colors = {
+      'ru pum pu ru rum': {
+         red: 'красный',
+         green: 'зеленый',
+         blue: 'синий',
+      },
+   };
 
-console.log(colors["ru pum pu ru rum"].red, colors["ru pum pu ru rum"].blue);
+   console.log(colors["ru pum pu ru rum"].red, colors["ru pum pu ru rum"].blue);
 }
 
 /*Task 5
@@ -75,22 +75,22 @@ console.log(colors["ru pum pu ru rum"].red, colors["ru pum pu ru rum"].blue);
 результат поместите в соответствующую переменную*/
 
 {
-let salaries = {
-   andrey: 500,
-   sveta: 413,
-   anton: 987,
-   pavel: 664,
-   alexandra: 199,
-}
+   let salaries = {
+      andrey: 500,
+      sveta: 413,
+      anton: 987,
+      pavel: 664,
+      alexandra: 199,
+   }
 
-let sum = 0;
-let average;
-for (let key in salaries) {
-   sum +=salaries[key];// Общая сумма
-   average = sum / Object.keys(salaries).length;//Object.keys(...).length количество ключей объекта
-}
+   let sum = 0;
+   let average;
+   for (let key in salaries) {
+      sum += salaries[key];// Общая сумма
+      average = sum / Object.keys(salaries).length;//Object.keys(...).length количество ключей объекта
+   }
 
-console.log(`Средняя зарплата сотрудников равна ${average}`);
+   console.log(`Средняя зарплата сотрудников равна ${average}`);
 }
 
 /*Task 6
@@ -101,63 +101,98 @@ console.log(`Средняя зарплата сотрудников равна $
 */
 
 {
-let login1 = prompt("Ваш логин?", "логин");
-let password1 = prompt("Ваш пароль?", 123);
+   let login1 = prompt("Ваш логин?", "логин");
+   let password1 = prompt("Ваш пароль?", 123);
 
-let user = {
-   userLog1: login1,
-   userPas1: password1,
+   let user = {
+      userLog1: login1,
+      userPas1: password1,
+   }
+
+   let login2 = prompt("Подтвердите логин.");
+   let password2 = prompt("Подтвердите.");
+
+   let user2 = {
+      userLog2: login2,
+      userPas2: password2,
+   }
+
+   if (user.userLog1 === user2.userLog2 && user.userPas1 === user2.userPas2) {
+      alert(`"Добро пожалоВать" ${user.userLog1}`)
+   } else if (user.userLog1 !== user2.userLog2 && user.userPas1 === user2.userPas2) {
+      alert(`"Не верно введён login"`)
+   } else if (user.userLog1 === user2.userLog2 && user.userPas1 !== user2.userPas2) {
+      alert(`"Не верно введён password"`)
+   } else if (user.userLog1 !== user2.userLog2 && user.userPas1 !== user2.userPas2) {
+      alert(`"Не верно введён password и login"`)
+   };
 }
 
-let login2 = prompt("Подтвердите логин.");
-let password2 = prompt("Подтвердите.");
-
-let user2 = {
-   userLog2: login2,
-   userPas2: password2,
-}
-
-if (user.userLog1 === user2.userLog2 && user.userPas1 === user2.userPas2) {
-   alert(`"Добро пожалоВать" ${user.userLog1}`)
-} else if (user.userLog1 !== user2.userLog2 && user.userPas1 === user2.userPas2) {
-   alert(`"Не верно введён login"`)
-} else if (user.userLog1 === user2.userLog2 && user.userPas1 !== user2.userPas2) {
-   alert(`"Не верно введён password"`)
-} else if (user.userLog1 !== user2.userLog2 && user.userPas1 !== user2.userPas2) {
-   alert(`"Не верно введён password и login"`)
-};
-}
-
-/*Task 1
+/*Task 1.1
 Мы на футбольном матче, счет забитых голов одной команды не может превышать 9 мячей. 
 Жаль что нам присылают результат матча в формате '2:5', ведь нам надо это вывести в консоль словами. 
 Давайте напишем программу которая будет за нас переводить формат и выводить результат в консоль.  
 */
 
-//Разбираюсь, надеюсь позже скину
+{
+   function gameScore(goal1, goal2) {
+      let number = {
+         0: "zero",
+         1: "one",
+         2: "two",
+         3: "three",
+         4: "fhour",
+         5: "five",
+         6: "six",
+         7: "seven",
+         8: "eight",
+         9: "nine",
+      };
+
+      console.log(`${number[goal1]} : ${number[goal2]}`);
+      return
+   }
+
+   gameScore(0, 9);
+}
 
 /*Task 1.2
 Даны два одинаковых обьекта. Сравните их так чтобы они были равны
 */
 
 {
-let student1 = {
-   name: 'Polina',
-   age: 27,
+   let student1 = {
+      name: 'Polina',
+      age: 27,
+   }
+
+   let student2 = {
+      name: 'Polina',
+      age: 27,
+   }
+
+   console.log(JSON.stringify(student1) === JSON.stringify(student2));//JSON.stringify(...) Конвертирует объекты в строку и сравнивает полученные строки. Минус: если свойства объектов идут не в одном порядке, то сравнение вернёт false.
 }
 
-let student2 = {
-   name: 'Polina',
-   age: 27,
-}
-
-console.log(JSON.stringify(student1)===JSON.stringify(student2));//JSON.stringify(...) Конвертирует объекты в строку и сравнивает полученные строки. Минус: если свойства объектов идут не в одном порядке, то сравнение вернёт false.
-}
-
-/*Task 3 
+/*Task 1.3 
 У нас есть обьект с животными. Наша задача узнать имя птицы и вывести его в консоль. 
 Но произошла ошибка и в этом обьекте нету птицы. Если попробуем узнать имя мы получим **ОШИБКУ**. 
 Задача придумать как обратиться к несуществующему обьекту и не получить ошибку чтобы наша программа работала дальше.
  */
 
-// Не разобрался(((
+// Разобрался, сразу пытался сделать чем-то сложным и вообще упустил из вида опциональную цепочку '?.'
+
+{
+   const animals = {
+      cat: {
+         name: "Енчик",
+         age: 3,
+      },
+      dog: {
+         name: "Орео",
+         age: 2,
+      },
+   };
+
+   console.log(animals?.bird?.name);
+}
