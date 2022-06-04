697846133229a0cf7999d08c6fa41337cd76e7b8
Artur-Gazaran <arturgazaran94@gmail.com>
2022-06-04 14:40:04 +0300

    fix(hw-6):Task-4 из аргументов убрал числа.Task-7. Advanced level.

--- home-work-6/hw-6.js
+++ home-work-6/hw-6.js
@@ -53,4 +53,69 @@ const binary = [0, 0, 0, 0]
 const str = binary.join('1');
 console.log(str);
 
+
 //Advanced
+
+//Task-1
+let palindrom = 'topot';
+function checkPalndrom(a) {
+	if (palindrom === palindrom.split('').reverse().join('')) {
+		console.log('Является палиндромом');
+	} else {
+		console.log('Не является палиндромом');
+	}
+}
+checkPalndrom(palindrom);
+
+//Task-2
+
+const matrix = [
+	[12, 98, 78, 65, 23],
+	[54, 76, 98, 43, 65],
+	[13, 324, 65, 312],
+	[9092, 22, 45, 90000],
+];
+
+let sumArray = 0;
+let sum = 0;
+let amount = 0;
+
+for (let num of matrix) {
+	for( let m of num) {
+	sum += m;
+	amount += 1;
+}
+}
+sumArray = sum / amount;
+console.log(sumArray);
+
+//Task-3
+
+const mixedNumbers = [-14, 24, -89, 43, 0 , -1, 412, 4];
+let plusNumbers = [];
+let minusNumbers = [];
+for (let i of mixedNumbers) {
+	if (i >= 0) {
+		plusNumbers.push(i);
+	} else {
+		minusNumbers.push(i);
+	}
+}
+console.log(plusNumbers);
+console.log(minusNumbers);
+
+
+//Task-4
+let randomNumber = [];
+let randomNumbers = [
+	Math.floor(Math.random() * 100),
+	Math.floor(Math.random() * 100),
+	Math.floor(Math.random() * 100),
+	Math.floor(Math.random() * 100),
+	Math.floor(Math.random() * 100),
+];
+for(let number of randomNumbers) {
+	randomNumber.push(Math.pow(number,3));
+}
+console.log(randomNumbers);
+console.log(randomNumber);
